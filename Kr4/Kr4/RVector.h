1c08aae8bddcc5c01bfe1aefe7fd4538d66f04e8
Houdini <davletshin2004@mail.ru>
2023-11-20 21:47:34 +0300

    First commit

---
+++ Kr4/Kr4/RVector.h
@@ -0,0 +1,21 @@
+// RVector.h
+#pragma once
+
+#include <iostream>
+
+class RVector {
+private:
+    double x, y, z;
+
+public:
+    RVector();
+    RVector(double x, double y, double z);
+
+    void display() const;
+
+    RVector operator+(const RVector& other) const;
+
+    RVector operator-(const RVector& other) const;
+
+    RVector operator*(const RVector& other) const;
+};
